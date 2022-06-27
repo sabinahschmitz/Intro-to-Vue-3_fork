913a40b4c6a9a880c0215596e91ae1afdfeeb9af
Sabina Schmitz <sabinahschmitz@gmail.com>
2022-06-27 14:22:20 +0200

    solve merge conflicts

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 const app = Vue.createApp({
-<<<<<<< HEAD
-    data()  {
-        return {
-        product: 'Socks',
-        description: 'Beautiful hand-knitted '
-    }
-=======
+    
     data() {
         return {
             product: 'Socks',
+            description: 'Beautiful hand-knitted ',
             image:'./assets/images/socks_green.jpg',
             url: 'https://brownleatherbook.com'
         }
->>>>>>> L3-start
     }
 })
