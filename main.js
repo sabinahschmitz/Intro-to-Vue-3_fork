e7bfdd6ddd66e0b9c685800cadc5713e88725f36
Sabina Schmitz <sabinahschmitz@gmail.com>
2022-06-28 21:03:25 +0200

    edits

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
             product: 'Socks',
             brand: 'Vue Mastery',
             selectedVariant: 0,
-            onSale: true,
+            onSale: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
